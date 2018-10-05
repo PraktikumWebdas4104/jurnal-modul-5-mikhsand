1d6920d347a9db8dd415da0cee9cdf677b9b6a86
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-10-05 10:27:51 +0700

    komentar.php
    komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,22 @@
-<?php
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" id="form2">
+	<table>
+		<tr>
+			<td>Kata</td>
+		</tr>
 
+		<tr>
+			<td><input type="text" name="kata"></td>
+		</tr>
+
+		<tr>
+			<td><input type="submit" name="submit" value="hitung"></td>
+		</tr>
+	</table>
+</form>
+
+<?php
+	if (isset($_POST['submit'])) {
+		$hitung=explode(" ", $_POST['kata']);
+		echo "ADA ".count($hitung)." KATA";
+	}
 ?>
