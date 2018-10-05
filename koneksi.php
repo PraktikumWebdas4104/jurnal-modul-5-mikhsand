650313e107415ce4c2c149e44ab66be4a089ef71
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-10-05 10:21:21 +0700

    koneksi.php
    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,21 @@
 <?php
 
+	if (isset($_POST['submit'])) {
+		$conn=new mysqli("localhost","root","","jurnal");
+			if (isset($_POST['submit'])) {
+					if (strlen($_POST['nim'])==10 && ($_POST['nama'])<25){
+							if ($conn) {
+								$nim=$_POST['nim'];
+								$nama=$_POST['nama'];
+								$email=$_POST['email'];		
+									$sql="INSERT INTO `akun` (`nim`,`nama`,`email`) VALUES ('$nim','$nama','$email')";
+									$conn->query($sql);
+										echo "input berhasil";
+							}
+					}else{
+						echo "input gagal, input kembali <a href='registrasi.php'>disini</a>";
+					}
+			}
+	}
+						
 ?>
