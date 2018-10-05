2c1fa0bf16ca05841e5082b836599a83485d131b
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-10-05 10:20:48 +0700

    prosesregis.php
    prosesregis

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,10 @@
 <?php
+	session_start();
 
-
+	if (!isset($_POST['nim']) and !isset($_POST['nama']) and !isset($_POST['email'])) {
+		echo "masukkan data terlebih dahulu <a href='registrasi.php'>input disini</a>";
+	}
+	else{
+		include("koneksi.php");
+	}
 ?>
