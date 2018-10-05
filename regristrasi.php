b6a6edc969708c2c769ce48d6785f793a2f5fc05
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-10-05 10:19:58 +0700

    registrasi.php
    registrasi

--- regristrasi.php
+++ regristrasi.php
@@ -4,8 +4,32 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
+	<h2>Form</h2>
 
+	<form action="prosesregis.php" method="POST">
+		<table>
+			<tr>
+				<td>NIM</td>
+				<td></td>
+				<td><input type="text" name="nim"></td>
+			</tr>
 
+			<tr>
+				<td>Nama</td>
+				<td></td>
+				<td><input type="text" name="nama"></td>
+			</tr>
 
+			<tr>
+				<td>E-mail</td>
+				<td></td>
+				<td><input type="email" name="email"></td>
+			</tr>
+
+			<tr>
+				<td><input type="submit" name="submit" value="send"></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
